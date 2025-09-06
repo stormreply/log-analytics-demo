c2cb923882ddb8bbdfd7eff469904ae2baacf0f1
Bernd Herding <bernd.herding@external.biontech.com>
2025-09-06 12:35:39 +0200

    remove aws_lakeformation_permissions

--- glue-catalog-database.tf
+++ glue-catalog-database.tf
@@ -9,20 +9,20 @@ data "external" "lakeformation_enabled" {
   ]
 }
 
-locals {
-  lakeformation_enabled = data.external.lakeformation_enabled.result.enabled == "true"
-}
+# locals {
+#   lakeformation_enabled = data.external.lakeformation_enabled.result.enabled == "true"
+# }
 
-resource "aws_lakeformation_permissions" "catalog_create_db" {
-  count       = local.lakeformation_enabled ? 1 : 0
-  principal   = "arn:aws:iam::${local.account_id}:role/slt-0-storm-library-for-terraform-deployment" # local.account_id
-  permissions = ["CREATE_DATABASE"]
+# resource "aws_lakeformation_permissions" "catalog_create_db" {
+#   count       = local.lakeformation_enabled ? 1 : 0
+#   principal   = "arn:aws:iam::${local.account_id}:role/slt-0-storm-library-for-terraform-deployment" # local.account_id
+#   permissions = ["CREATE_DATABASE"]
 
-  catalog_resource = true
-}
+#   catalog_resource = true
+# }
 
 resource "aws_glue_catalog_database" "glue_database" {
   name = var.deployment.name
 
-  depends_on = [aws_lakeformation_permissions.catalog_create_db]
+  # depends_on = [aws_lakeformation_permissions.catalog_create_db]
 }
