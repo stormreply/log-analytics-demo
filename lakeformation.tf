9f6b47f02948d73a431092752f48a1bf35ca4f28
Bernd Herding <bernd.herding@external.biontech.com>
2025-09-15 21:18:40 +0200

    set lf:ALL permission on hive database

--- lakeformation.tf
+++ lakeformation.tf
@@ -9,27 +9,9 @@ resource "aws_lakeformation_permissions" "zeppelin_database" {
 
 resource "aws_lakeformation_permissions" "hive_database" {
   principal   = aws_iam_role.zeppelin_notebook.arn
-  permissions = ["DESCRIBE"]
+  permissions = ["ALL"]
   database {
     name       = aws_glue_catalog_database.hive_database.name
     catalog_id = data.aws_caller_identity.current.account_id
   }
 }
-
-# resource "aws_lakeformation_permissions" "ingestion_stream_table" {
-#   principal   = "arn:aws:iam::541792499640:role/slt-8-log-analytics-demo-berndherding-zeppelin-notebook"
-#   permissions = ["SELECT", "DESCRIBE"]
-
-#   table {
-#     database_name = "slt-8-log-analytics-demo-berndherding"
-#     name          = "ingestion_stream"
-#   }
-# }
-
-# resource "aws_lakeformation_permissions" "catalog_create_db" {
-#   count       = local.lakeformation_enabled ? 1 : 0
-#   principal   = "arn:aws:iam::${local.account_id}:role/slt-0-storm-library-for-terraform-deployment" # local.account_id
-#   permissions = ["CREATE_DATABASE"]
-
-#   catalog_resource = true
-# }
