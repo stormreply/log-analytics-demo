585d44fbee8d8f78a0e39ce26a7c7deceae55837
Bernd Herding <bernd.herding@external.biontech.com>
2025-11-09 22:23:30 +0100

    use ssm-managed-instance

--- producer-instance.tf
+++ producer-instance.tf
@@ -2,7 +2,7 @@ module "producer" {
   # checkov:skip=CKV_TF_1: "Ensure Terraform module sources use a commit hash"
   # checkov:skip=CKV_TF_2: "Ensure Terraform module sources use a tag with a version number"
   # tflint-ignore: terraform_module_pinned_source
-  source                 = "git::https://github.com/stormreply/terraform-build-controller.git"
+  source                 = "git::https://github.com/stormreply/ssm-managed-instance.git"
   name                   = "${local._name_tag}-producer"
   instance_type          = "m6i.xlarge"
   policies               = local.producer_policies
